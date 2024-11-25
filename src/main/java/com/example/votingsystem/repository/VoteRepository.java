1b8dfd8f1a689838950ca0aff88ea2e7421befa9
Mikael <mikael.quadros@gmail.com>
2024-11-25 02:29:33 -0300

    add: method to VoteRepository to check existence of a vote by session and user ID

--- src/main/java/com/example/votingsystem/repository/VoteRepository.java
+++ src/main/java/com/example/votingsystem/repository/VoteRepository.java
@@ -3,4 +3,6 @@ package com.example.votingsystem.repository;
 import com.example.votingsystem.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoteRepository extends JpaRepository<Vote, Integer> {}
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+  Boolean existsByVoteSessionIdAndUserId(Integer voteSessionId, Integer userId);
+}
